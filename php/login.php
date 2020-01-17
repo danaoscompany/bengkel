a1cab503d85448b1c1c1bc246803d54e749e068d
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-17 16:38:04 +0700

    Latest commit

--- php/login.php
+++ php/login.php
@@ -6,15 +6,16 @@ $results = $c->query("SELECT * FROM users WHERE uid='" . $uid . "'");
 if ($results && $results->num_rows > 0) {
 	$row = $results->fetch_assoc();
 	if (intval($row["account_type"]) == 1 && intval($row["minimum_amount_paid"]) == 0) {
-		echo -2;
+		echo "{\"response\": -2, \"user_id\": " . $row["id"] . "}";
 	} else {
 	    session_id("bengkel");
 	    session_start();
 	    $_SESSION["logged_in"] = true;
 	    $_SESSION["user_id"] = $row["id"];
-		echo $row["id"];
+		echo "{\"response\": 0, \"user_id\": " . $row["id"] . "}";
 	}
 } else {
 	$c->query("INSERT INTO users (phone, uid) VALUES ('" . $phone . "', '" . $uid . "')");
-	echo -1;
+	$id = mysqli_insert_id($c);
+	echo "{\"response\": -1, \"user_id\": " . $id . "}";
 }
