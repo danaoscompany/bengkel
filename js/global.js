8ae9e9072ec25633f5eaf82b499772dc8ea6231f
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-17 13:45:41 +0700

    Latest commit

--- js/global.js
+++ js/global.js
@@ -2,3 +2,16 @@ const PROTOCOL = "https";
 const HOST = "wartekgroup.co.id/bengkel";
 //const HOST = "localhost/bengkel";
 const PHP_URL = PROTOCOL+"://"+HOST+"/php/";
+
+function logout() {
+    localStorage.setItem("user_id", 0);
+    $.ajax({
+        type: 'GET',
+        url: PHP_URL+'logout.php',
+        dataType: 'text',
+        cache: false,
+        success: function(response) {
+            window.location.href = "login.html";
+        }
+    });
+}
