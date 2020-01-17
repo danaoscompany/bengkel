b0f589dc0e057803fff990115473787ae87291c0
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-17 17:05:45 +0700

    Latest commit

--- php/add-progress.php
+++ php/add-progress.php
@@ -15,7 +15,7 @@ if ($orders && $orders->num_rows > 0) {
 		$url = 'https://fcm.googleapis.com/fcm/send';
     	$fields = array (
             'registration_ids' => array (
-                    $fcmID
+                    "dxb7XyB_eGY:APA91bEQz17RgEZgN6iYAWpGEKwOWRzEZEkAzrWWViPE_u3qDZ8bMasASQxYJCWl8YL1LL6fkJmM0mXo543j0Ntqm5Womb0XbSY0kOlWpGPV9CjqaXhWrogToFoM7EPPAYn2HweTIeSJ"
             ),
             'data' => array (
                     "type" => "2",
