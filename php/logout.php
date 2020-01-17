c327acc4fbd56a08bd58da49ef6501d0787d7956
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-17 13:35:00 +0700

    Latest commit

--- php/logout.php
+++ php/logout.php
@@ -1,1 +1,8 @@
 <?php
+session_id("bengkel");
+session_start();
+$_SESSION["user_id"] = 0;
+$_SESSION["logged_in"] = false;
+unset($_SESSION["user_id"]);
+unset($_SESSION["logged_in"]);
+session_destroy();
