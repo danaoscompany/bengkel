9865e058c13b24417ab106651fb7c8fb522749df
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-21 00:43:21 +0700

    Latest commit

--- php/update-profile.php
+++ php/update-profile.php
@@ -10,5 +10,4 @@ if (!file_exists('../userdata/profile_pictures')) {
     mkdir('../userdata/profile_pictures', 0777, true);
 }
 move_uploaded_file($_FILES["file"]["tmp_name"], "../userdata/profile_pictures/" . $_FILES["file"]["name"]);
-$c->query("UPDATE users SET name='" . $name . "', account_type=" . $accountType . ", version=" . $version . ", username='" . $username . "', profile_picture='profile_pictures/" . $_FILES["file"]["name"] . "', email='" . $email . "' WHERE uid='" . $uid . "'");
-echo "UPDATE users SET name='" . $name . "', account_type=" . $accountType . ", version=" . $version . ", username='" . $username . "', profile_picture='profile_pictures/" . $_FILES["file"]["name"] . "', email='" . $email . "' WHERE uid='" . $uid . "'";
+$c->query("UPDATE users SET name='" . $name . "', account_type=" . $accountType . ", version=" . $version . ", username='" . $username . "', profile_picture='profile_pictures/" . $_FILES["file"]["name"] . "', email='" . $email . "' WHERE uid='" . $uid . "', profile_set_up=1 WHERE uid='" . $uid . "'");
