d4914846828cb5e4802eae331f6c3c3eee5124d4
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-21 07:33:26 +0700

    Latest commit

--- php/add-schedule.php
+++ php/add-schedule.php
@@ -2,6 +2,8 @@
 include 'db.php';
 $orderID = intval($_POST["order_id"]);
 $complaintID = intval($_POST["complaint_id"]);
+$technicianID = intval($_POST["technician_id"]);
 $type = intval($_POST["type"]); //1 = order, 2 = complaint
 $date = $_POST["date"];
 $c->query("INSERT INTO schedules (order_id, complaint_id, type, date) VALUES (" . $orderID . ", " . $complaintID . ", " . $type . ", '" . $date . "')");
+$c->query("UPDATE complaints SET technician_id=" . $technicianID . " WHERE id=" . $complaintID);
