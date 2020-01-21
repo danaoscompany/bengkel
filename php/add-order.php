1e825dca89b9c9b85611f6cbeb83540ac5b1b99a
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-21 12:27:56 +0700

    Latest commit

--- php/add-order.php
+++ php/add-order.php
@@ -3,6 +3,7 @@ include 'db.php';
 $address = $_POST["address"];
 $orderID = $_POST["order_id"];
 $userID = intval($_POST["user_id"]);
+$units = intval($_POST["units"]);
 $kind = intval($_POST["kind"]);
 $type = intval($_POST["type"]);
 $capacity = floatval($_POST["capacity"]);
@@ -19,7 +20,7 @@ $endResultApproval = intval($_POST["end_result_approval"]);
 $paymentMethod = intval($_POST["payment_method"]);
 $orderType = intval($_POST["order_type"]);
 $complaint = $_POST["complaint"];
-$sql = "INSERT INTO orders (user_id, order_id, order_type, address, kind, type, capacity, material, complaint, latitude, longitude, price_estimation, technical_estimation, date, progress, price_change_approval, end_result_approval, payment_method, done) VALUES (" . $userID . ", '" . $orderID . "', " . $orderType . ", '" . $address . "', " . $kind . ", " . $type . ", " . $capacity . ", " . $material . ", '" . $complaint . "', " . $latitude . ", " . $longitude . ", " . $priceEstimation . ", " . $technicalEstimation . ", '" . $date . "', " . $progress . ", " . $priceChangeApproval . ", " . $endResultApproval . ", " . $paymentMethod . ", 0)";
+$sql = "INSERT INTO orders (user_id, order_id, order_type, address, units, kind, type, capacity, material, complaint, latitude, longitude, price_estimation, technical_estimation, date, progress, price_change_approval, end_result_approval, payment_method, done) VALUES (" . $userID . ", '" . $orderID . "', " . $orderType . ", '" . $address . "', " . $units. ", " . $kind . ", " . $type . ", " . $capacity . ", " . $material . ", '" . $complaint . "', " . $latitude . ", " . $longitude . ", " . $priceEstimation . ", " . $technicalEstimation . ", '" . $date . "', " . $progress . ", " . $priceChangeApproval . ", " . $endResultApproval . ", " . $paymentMethod . ", 0)";
 $c->query($sql);
 $orderID = mysqli_insert_id($c);
 foreach ($progresses as $progressItem) {
