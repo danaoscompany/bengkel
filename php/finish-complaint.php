ad797f4f63fbb328715d294fb4cf308772afb354
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-21 10:06:48 +0700

    Latest commit

--- php/finish-complaint.php
+++ php/finish-complaint.php
@@ -33,5 +33,6 @@ if ($results && $results->num_rows > 0) {
     	curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
     	$result = curl_exec ( $ch );
     	curl_close ( $ch );
+    	echo $fcmID;
 	}
 }
