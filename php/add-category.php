59f4053f378f55393088aa06e4524eb7dd4cdcd0
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-25 16:17:53 +0700

    Latest commit

--- php/add-category.php
+++ php/add-category.php
@@ -10,3 +10,4 @@ session_start();
 $adminID = $_SESSION["user_id"];
 $adminName = $c->query("SELECT * FROM admins WHERE id=" . $adminID)->fetch_assoc()["name"];
 sendLog($c, $adminID, 0, 'Admin dengan nama ' . $adminName . ' menambahkan kategori baru: ' . $name);
+echo "INSERT INTO categories (name, icon) VALUES ('" . $name . "', '" . $_FILES["file"]["name"] . "')";
