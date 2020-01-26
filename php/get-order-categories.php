72570bc7121edbf69ede9bb8eaf75be8f1c8916d
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-01-26 14:17:28 +0700

    Latest commit

--- php/get-order-categories.php
+++ php/get-order-categories.php
@@ -2,16 +2,10 @@
 include 'db.php';
 $categoryID = intval($_POST["category_id"]);
 $categories = [];
-while (true) {
 	$results = $c->query("SELECT * FROM categories WHERE id=" . $categoryID);
 	if ($results && $results->num_rows > 0) {
 		$row = $results->fetch_assoc();
 		array_push($categories, $row);
 		$categoryID = intval($row["parent_id"]);
-		if ($categoryID == 0) {
-			break;
-		}
 	}
-	break;
-}
 echo json_encode($categories);
